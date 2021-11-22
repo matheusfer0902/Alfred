68daf55479b29cb23117b272ae88bfbc1a9c3510
Matheus Ferreira <matheusfer0902@gmail.com>
2021-11-22 09:55:57 -0300

    Update Delivery.hpp
    Atualizando preços

--- Delivery.hpp
+++ Delivery.hpp
@@ -1,27 +1,35 @@
-#ifndef DELIVERY_H
-#define DELIVERY_H
+#ifndef PEDIDO_H
+#define PEDIDO_H
 #pragma once
-	
-#include "Cliente.hpp"
+
+#define MAX_PRATOS 10
+
+#include <string>
 
 namespace pkt_comanda {
 
-class Delivery: public Cliente
+class Pedido  
 {
 	private:
-
-		std::string contato, endereco;
+	
+		int quantidade, prato;
+		std::string cardapio[MAX_PRATOS] {"Executivo", "Churrasco", "Sopa suspeita", "Cafe", "Suco", 
+		"Feijoada", "Salada", "Pipoca na manteiga", "Pizza", "Sushi"};
+		double preco[MAX_PRATOS] {14.99, 24.99, 99.99, 4.99, 4.99, 21.75, 9.99, 1.99, 31.47, 2.99};
 
 	public:
 
-		Delivery();
+		Pedido();
+		~Pedido();
 
-		void setContato(std::string tell);
-		void setEndereco(std::string end);
+		void setQuantidade(int q);
+		void setPrato(int p);
 
-		std::string getContato();
-		std::string getEndereco();
+		int getQuantidade();
+		int getPrato();
+		std::string getStrPrato();
 
 };
+
 };
 #endif
